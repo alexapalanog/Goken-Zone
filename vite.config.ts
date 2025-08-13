b605672302bf2e9b39f20e194e87b8eb050ad8a0
alexapalanog <alexapalanog@gmail.com>
2025-08-13 20:07:51 +0800

    Configure correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // THIS IS THE CRITICAL LINE TO ADD
-      base: '/Gokenzone/', 
+      // THIS MUST BE EXACTLY RIGHT
+      base: '/Goken-Zone/', 
       
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
