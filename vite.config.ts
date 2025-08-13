e585ef657b279ad08da53894e664feb8a62053ce
alexapalanog <alexapalanog@gmail.com>
2025-08-13 19:46:12 +0800

    Configure project for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // THIS IS THE IMPORTANT LINE TO ADD
+      base: '/Gokenzone/', 
+      
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
